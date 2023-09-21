104e49938d163f503065a0ec9dbda04d362675d5
Sergey <sergey211@gmail.com>
2023-09-21 12:51:22 +0200

    all tests added

--- tests/test_create_meme.py
+++ tests/test_create_meme.py
@@ -2,8 +2,7 @@ import pytest
 from endpoints.post_meme_endpoint import PostMemeEndpoint
 
 
-# @pytest.mark.skip('for testing skipping') # if we skip this test - we getting an error 500
-# @pytest.mark.critical
+# @pytest.mark.skip('for testing skipping') # if we don't skip this test - we getting an error 500
 def test_add_new_meme(token):
     endpoint_meme = PostMemeEndpoint(token)
     endpoint_meme.post_meme()
@@ -11,8 +10,6 @@ def test_add_new_meme(token):
     assert endpoint_meme.code_is_the_same_as_custom()
 
 
-@pytest.mark.skip('for testing skipping')
-@pytest.mark.critical
 def test_add_new_meme_with_bad_data(token):
     data = {"text": "test", "tags": ["test"], "info": {"key1": "key2"}}
     endpoint_meme = PostMemeEndpoint(token, data)
